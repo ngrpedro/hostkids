ad342c3e1a18c6ef9aa765ceff4d2e44108c4aaf
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-22 20:57:40 -0300

    maps

--- src/components/AllPinsMap.tsx
+++ src/components/AllPinsMap.tsx
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import mapIcon from "../utils/mapIcon";
 
 const AllPinsMap = () => {
-  const position = [51.505, -0.09];
+  const position: [number, number][] = [
+    [-21.1806395, -50.4204816],
+    [-21.1916682, -50.4420556],
+    [-21.1967098, -50.4468621],
+  ];
 
   return (
     <div className="w-[300px]">
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={[-21.1839454, -50.4384078]}
+        zoom={14}
         scrollWheelZoom={false}
         style={{ width: "100vw", height: "100vh" }}
       >
@@ -17,32 +21,36 @@ const AllPinsMap = () => {
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGVkcm8zOTkiLCJhIjoiY2w5ZzFpODZmMnY3dDQxbXZxems2cDYzMiJ9.ZO5_rVpmfVgQzIBfGCFN0g`}
         />
 
-        <Marker position={[51.505, -0.09]} icon={mapIcon}>
-          <Popup
-            closeButton={false}
-            minWidth={240}
-            maxWidth={240}
-            className={"font-bold"}
-          >
-            Lar Acolhida
-            <Link to="/InstituteProfile">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+        {position.map((loc, index) => {
+          return (
+            <Marker key={index} position={loc} icon={mapIcon}>
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className={"font-bold"}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                />
-              </svg>
-            </Link>
-          </Popup>
-        </Marker>
+                Lar Acolhida
+                <Link to="/InstituteProfile">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
