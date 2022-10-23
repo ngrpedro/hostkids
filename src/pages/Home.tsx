c6b02d00ecd1b6a062d3434cd9700c92c1fed04d
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-22 21:52:04 -0300

    maps

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,30 @@ import LogoWhite from "../assets/LogoWhite";
 
 const Home = () => {
   return (
-    <div className="p-8 sm:px-20 py-10 text-white bg-[#0089A5] h-[100vh]
-                    flex flex-col justify-between">
+    <div
+      className="p-8 sm:px-20 py-10 text-white bg-[#0089A5] h-[100vh]
+                    flex flex-col justify-between"
+    >
       <div className="space-y-3">
         <div className="flex items-center justify-between">
-          <LogoWhite boxSize={"108"} />
-          <div>
-            <p className="font-extrabold text-xl md:text-[36px] md:leading-[42px]">
-              Araçatuba
-            </p>
-            <p className="font-semibold md:text-[20px] md:leading-[34px]">
-              São Paulo
-            </p>
+          <div className="flex items-center gap-5">
+            <LogoWhite boxSize={"108"} />
+            <div>
+          <p className="font-extrabold text-xl md:text-[36px] md:leading-[42px]">
+            Araçatuba
+          </p>
+          <p className="font-semibold md:text-[20px] md:leading-[34px]">
+            São Paulo
+          </p>
+        </div>
           </div>
+
+          <Link
+            to="/InstituteView"
+            className="bg-[#FFD666] p-3 rounded-full animate-pulse"
+          >
+            <ArrowFatLinesRight className="text-cyan-900" />
+          </Link>
         </div>
 
         <div className="flex flex-col lg:flex-row items-start justify-between gap-5">
@@ -39,13 +50,6 @@ const Home = () => {
           Tenha acesso as informações de instituições de acolhimento infantil da
           sua cidade e mude o dia de muitas crianças
         </p>
-
-        <Link
-          to="/InstituteView"
-          className="bg-[#FFD666] p-3 rounded-full animate-pulse"
-        >
-          <ArrowFatLinesRight className="text-cyan-900" />
-        </Link>
       </div>
     </div>
   );
