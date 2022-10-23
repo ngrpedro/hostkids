dcebef46f136c532e2961f9be30cd49eec6fe784
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-22 21:25:04 -0300

    maps

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,7 +4,7 @@ import LogoWhite from "../assets/LogoWhite";
 const TopBar = () => {
   return (
     <div
-      className="absolute top-0 bg-[#0089A5] rounded-2xl z-50 w-8/12 m-12 p-4
+      className="absolute bottom-0 bg-[#0089A5] rounded-2xl z-50 w-8/12 m-12 p-4
                 flex items-center gap-3"
     >
       <Link to="/">
