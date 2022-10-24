985756b581716f18e8f552bd88f645a4cc793b7a
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-24 17:14:12 -0300

    warningmap

--- src/pages/InstituteView.tsx
+++ src/pages/InstituteView.tsx
@@ -1,9 +1,15 @@
+import { Warning } from "phosphor-react";
 import AllPinsMap from "../components/AllPinsMap";
 
 const InstituteView = () => {
   return (
-    <div>
+    <div className="relative">
       <AllPinsMap />
+      <div className="absolute z-[10000] top-10 left-10 md:left-40 p-4 bg-[#0089A5] rounded-lg text-white
+                  flex items-center justify-center gap-4 text-lg font-medium">
+        <Warning size={28} />
+        Selecione no mapa a instituição para saber mais!!
+      </div>
     </div>
   );
 };
