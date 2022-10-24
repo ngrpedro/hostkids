a3b3af34cff8073afc9f06fd853a7122a28d81b2
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-24 17:04:15 -0300

    navbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,7 +6,7 @@ import LogoWhite from "../assets/LogoWhite";
 const TopBar = () => {
   return (
     <div
-      className="bg-[#0089A5] p-4 md:px-14 md:py-5 group
+      className="bg-[#0089A5] p-4 md:px-14 md:py-5 group delay-100
                 flex items-center justify-between gap-3 hover:bg-white transition-colors hover:shadow-2xl"
     >
       <div className="flex items-center gap-3 ">
