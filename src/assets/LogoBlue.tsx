7fe93745b3ff8f0264407654c1539283ae372a59
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-24 17:02:08 -0300

    navbar

---
+++ src/assets/LogoBlue.tsx
@@ -0,0 +1,46 @@
+interface LogoProps {
+    boxSize: string;
+  }
+
+function LogoBlue({ boxSize } : LogoProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={boxSize}
+      height={boxSize}
+      fill="none"
+      viewBox="0 0 128 128"
+    >
+      <path
+        stroke="#0089A5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="9"
+        d="M47.35 13.55a26 26 0 0117.8 48.85l-17.8-48.85z"
+      ></path>
+      <path
+        stroke="#0089A5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="9"
+        d="M13.55 72.65a26 26 0 0148.85-17.8l-48.85 17.8z"
+      ></path>
+      <path
+        stroke="#0089A5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="9"
+        d="M72.65 106.45a25.997 25.997 0 01-32.899-15.68A26 26 0 0154.85 57.6l17.8 48.85z"
+      ></path>
+      <path
+        stroke="#0089A5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="9"
+        d="M106.45 47.35a25.997 25.997 0 01-15.68 32.899A26 26 0 0157.6 65.15l48.85-17.8zM82.25 82.25L112 112"
+      ></path>
+    </svg>
+  );
+}
+
+export default LogoBlue;
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,24 +1,45 @@
-import { Plus } from "phosphor-react";
+import { MapTrifold, PaperPlaneTilt } from "phosphor-react";
 import { Link } from "react-router-dom";
+import LogoBlue from "../assets/LogoBlue";
 import LogoWhite from "../assets/LogoWhite";
 
 const TopBar = () => {
   return (
     <div
-      className="bg-[#0089A5] p-4
-                flex items-center justify-between gap-3 md:px-14"
+      className="bg-[#0089A5] p-4 md:px-14 md:py-5 group
+                flex items-center justify-between gap-3 hover:bg-white transition-colors hover:shadow-2xl"
     >
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 ">
         <Link to="/">
-          <LogoWhite boxSize={"38"} />
+          <div className="group-hover:hidden">
+            <LogoWhite boxSize={"38"} />
+          </div>
+          <div className="hidden group-hover:block">
+            <LogoBlue boxSize={"38"} />
+          </div>
         </Link>
-        <p className="font-semibold text-sm text-white ">
-          Selecione a instituição que deseja visitar :)
+        <p className="font-semibold text-xl text-white group-hover:text-[#0089A5]">
+          HOSTKIDS
         </p>
       </div>
-      <Link to="/InstituteProfile" className="bg-[#FFD666] p-2 rounded-full">
-        <Plus size={18} className="text-[#0089A5]" />
-      </Link>
+      <div className="flex gap-2">
+        <Link
+          to="/InstituteView"
+          className="rounded-md p-2 px-6 text-sm flex items-center justify-center bg-white text-[#11505c] gap-4
+                group-hover:bg-[#0089A5] group-hover:text-white transition-colors"
+        >
+          <MapTrifold size={28} />
+          Ver tudo no mapa
+        </Link>
+        <Link
+          to="/InstituteView"
+          className="rounded-md p-2 px-6 text-sm flex items-center justify-center bg-white text-[#11505c] gap-2
+                group-hover:bg-[#0089A5] group-hover:text-white transition-colors"
+        >
+          <PaperPlaneTilt size={20} />
+          Deseja cadastrar uma instituição?
+        </Link>
+      </div>
     </div>
   );
 };
