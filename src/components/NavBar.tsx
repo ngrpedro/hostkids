6634c3db8377ebacb5d371aa4cd5075c8743a28a
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-25 17:07:58 -0300

    newid

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,21 +7,30 @@ const NavBar = () => {
       className="bg-[#F6EFD9] p-4 md:px-14 md:py-5 group delay-100
               flex items-center justify-between gap-3 border-b-2 border-black box-shadow-black"
     >
-      <img src={bola} alt="" className="w-14" />
+      <Link to="/">
+        <img src={bola} alt="" className="w-14" />
+      </Link>
       <div className="flex items-center justify-between gap-3">
         <Link
           to="/InstituteView"
-          className=" bg-[#FFBD12] p-4 rounded-2xl border-2 border-black font-extrabold
+          className=" bg-[##FFC7DE] p-[10px] rounded-full border-2 border-black font-extrabold
                               box-shadow-black flex items-center justify-center gap-3"
         >
           Ver mapa
         </Link>{" "}
         <Link
           to="/InstituteView"
-          className=" bg-[#FFBD12] p-4 rounded-2xl border-2 border-black font-extrabold
+          className=" bg-[##FFC7DE] p-[10px] rounded-full border-2 border-black font-extrabold
         box-shadow-black flex items-center justify-center gap-3"
         >
           Nova instituição
+        </Link>
+        <Link
+          to="/InstituteView"
+          className=" bg-[##FFC7DE] p-[10px] rounded-full border-2 border-black font-extrabold
+        box-shadow-black flex items-center justify-center gap-3"
+        >
+          Fale com a gente
         </Link>
       </div>
     </div>
