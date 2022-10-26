322b75351b820c6a11c678f666e3d8dd10cb0f64
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-25 21:59:36 -0300

    justs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0089A5] p-10 flex flex-col items-center justify-center gap-4">
-      <p className="text-[14px] text-white">Powered by:: Pedro Soares</p>
+    <footer className="bg-[#F6EFD9] p-10 flex flex-col items-center justify-center gap-4">
+      <p className="text-[14px] text-[#1947E5]">Powered by:: Pedro Soares</p>
       <div className="flex items-center justify-center gap-5">
         <Link to="https://www.linkedin.com/in/pedro-nogueira399/">
-          <LinkedinLogo size={28} color="#fff" />
+          <LinkedinLogo size={28} color="#1947E5" />
         </Link>
         <Link to="https://github.com/ngrpedro">
-          <GithubLogo size={28} color="#fff" />
+          <GithubLogo size={28} color="#1947E5" />
         </Link>
       </div>
     </footer>
