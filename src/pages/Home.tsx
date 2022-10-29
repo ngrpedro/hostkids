0f316d327e322793b8ec5ccc404b987ea8fd36eb
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-29 15:53:24 -0300

    institutes responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       className="p-8 sm:px-14 py-10 text-gray-900 bg-white lg:h-[100vh]
                     flex flex-col justify-between absolute top-0 inset-x-0 gap-10 md:gap-0"
     >
-      <div className="space-y-12">
+      <div>
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-5">
             {/* <img src={bola} alt="" className="w-14" /> */}
@@ -26,10 +26,10 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="flex flex-col lg:flex-row items-start justify-between gap-20">
+        <div className="flex flex-col lg:flex-row items-start justify-between gap-20 my-auto h-screen lg:h-[70vh]">
           <div className="flex flex-col items-start gap-10 m-auto">
             <p
-              className="mt-5 font-extrabold text-[34px] md:text-[34px] max-w-[484px]
+              className="font-extrabold text-[34px] md:text-[34px] max-w-[484px]
             leading-[55px] sm:leading-[56px] tracking-[0.03em]"
             >
               Encontre instituições de acolhimento infantil
--- src/pages/InstituteView.tsx
+++ src/pages/InstituteView.tsx
@@ -1,11 +1,12 @@
+import { Info } from "phosphor-react";
 import AllInstitutes from "../components/AllInstitutes";
 import AllPinsMap from "../components/AllPinsMap";
 
 const InstituteView = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-      <div className="col-span-2 h-72 grid grid-cols-1 md:grid-cols-2">
-        <div className="flex flex-col items-start gap-10 m-auto  p-10">
+      <div className="col-span-2 lg:h-72 grid grid-cols-1 md:grid-cols-2 p-10 gap-8">
+        <div className="flex flex-col items-start gap-8 m-auto">
           <p className="font-extrabold text-[18px] md:text-[24px] sm:leading-[36px] tracking-[0.03em]">
             Encontre instituições de acolhimento infantil
           </p>
@@ -13,6 +14,26 @@ const InstituteView = () => {
             Tenha acesso as informações de instituições de acolhimento infantil
             da sua cidade e mude o dia de muitas crianças
           </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 items-center gap-5 m-auto md:pl-4">
+          <div className="border border-black rounded-lg p-4 md:p-6">
+            <Info size={28} />
+            <p className="mt-5 font-semibold text-[20px] leading-[40px]">
+              Aracatuba
+            </p>
+            <p className="font-normal text-sm leading-[28px]">
+              Fugiat elit dolore excepteur.
+            </p>
+          </div>
+          <div className="border border-black rounded-lg p-4 md:p-6">
+            <Info size={28} />
+            <p className="mt-5 font-semibold text-[20px] leading-[40px]">
+              Estatísticas
+            </p>
+            <p className="font-normal text-sm leading-[28px]">
+              Eu ea consequat occaecat.
+            </p>
+          </div>
         </div>
       </div>
       <AllInstitutes />
