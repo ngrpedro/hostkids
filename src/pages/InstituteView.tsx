adbae489bb5e79f23589d1090f6776ce028276d9
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-31 15:40:04 -0300

    institues

--- src/pages/InstituteView.tsx
+++ src/pages/InstituteView.tsx
@@ -7,8 +7,8 @@ const InstituteView = () => {
   const [mapView, setMapView] = useState(true);
 
   return (
-    <div className="h-screen space-y-10 mb-16">
-      <div className="px-4 py-10 md:px-10  bg-[#F4F5F7] col-span-2 lg:h-72 grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div className="space-y-10 mb-16">
+      <div className="px-4 py-10 md:px-10  bg-[#F4F5F7] col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="flex flex-col items-start gap-8 m-auto">
           <p className="text-[34px] font-bold leading-[54px] text-black">
             Encontre instituições e entidades em Araçatuba
@@ -18,8 +18,9 @@ const InstituteView = () => {
             da sua cidade e mude o dia de muitas crianças
           </p>
         </div>
-        <div className="">
-          <div className="flex items-center justify-center border border-black rounded-lg p-4 md:p-6 bg-white w-full h-full">
+        <div className="min-h-[15rem]">
+          <div className="flex items-center justify-center border border-black 
+                        rounded-lg p-4 md:p-6 bg-gray-50 w-full h-full">
             <Image size={68} />
           </div>
         </div>
