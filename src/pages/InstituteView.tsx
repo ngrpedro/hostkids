8ae667cef598ebdb20fb7d7c1e033759500a556d
Pedro Nogueira <ngrpedro22@gmail.com>
2022-10-31 14:53:53 -0300

    institues

--- src/pages/InstituteView.tsx
+++ src/pages/InstituteView.tsx
@@ -1,4 +1,4 @@
-import { Info } from "phosphor-react";
+import { Image } from "phosphor-react";
 import { useState } from "react";
 import AllInstitutes from "../components/AllInstitutes";
 
@@ -17,24 +17,9 @@ const InstituteView = () => {
             da sua cidade e mude o dia de muitas crianças
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 items-center gap-5 md:m-auto md:pl-4">
-          <div className="border border-black rounded-lg p-4 md:p-6 bg-white">
-            <Info size={28} />
-            <p className="mt-5 font-semibold text-[20px] leading-[40px]">
-              Aracatuba
-            </p>
-            <p className="font-normal text-sm leading-[28px]">
-              Fugiat elit dolore excepteur.
-            </p>
-          </div>
-          <div className="border border-black rounded-lg p-4 md:p-6 bg-white">
-            <Info size={28} />
-            <p className="mt-5 font-semibold text-[20px] leading-[40px]">
-              Estatísticas
-            </p>
-            <p className="font-normal text-sm leading-[28px]">
-              Eu ea consequat occaecat.
-            </p>
+        <div className="">
+          <div className="flex items-center justify-center border border-black rounded-lg p-4 md:p-6 bg-white w-full h-full">
+            <Image size={68} />
           </div>
         </div>
       </div>
